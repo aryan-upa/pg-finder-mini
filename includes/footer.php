ea0e6827650e95e757c2b3258934d7190d737c50
priyanshi-mittal <80943199+priyanshi-mittal@users.noreply.github.com>
2022-11-29 16:33:38 +0530

    created footer include

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city link">
+                <a href="property_list.php?city=Delhi">PG in Delhi</a>
+            </div>
+            <div class="footer-city link">
+                <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+            </div>
+            <div class="footer-city link">
+                <a href="property_list.php?city=Gurugram">PG in Gurugram</a>
+            </div>
+            <div class="footer-city link">
+                <a href="property_list.php?city=Mathura">PG in Mathura</a>
+            </div>
+        </div>
+        <div class="footer-copyright">&#169; GLA University 2022</div>
+    </div>
+</div>
+
+<script type="text/javascript" src="/js/jquery.js"></script>
+<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/js/common.js"></script>
